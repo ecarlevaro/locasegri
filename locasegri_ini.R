d5770f3e50d866eeb3cf0a910a5977097b1c0774
Emi Carlevaro <39544635+ecarlevaro@users.noreply.github.com>
2024-01-28 12:17:32 +1030

    First run

--- locasegri_ini.R
+++ locasegri_ini.R
@@ -1,3 +1,12 @@
+library(logr)
+library(data.table)
+library(future.apply)
+
+
+logFileName <- paste0(OUTPATH, 'log_', datetime_Stamp())
+log_open(logFileName, logdir=FALSE)
+log_print(paste0("Log initialized. RESULTID is ", RESULTID))
+
 # GENERATE OBJECTS ----
 
 # This function create a series of objects that are shared by all search functions.
@@ -6,69 +15,75 @@
 
 ## Verify global objects ----
 NPARAMS = NROW(PARAMS_CFG)
+T  = NROW(Y)
 if (!exists('NEQS', mode='numeric')) {
-  stop('Howe many equatiosn?') }
+  stop('How many equatiosn?') }
 
 if (!exists('Y', mode='numeric')) {
-  stop('Objects Y and X should exist before calling efisegri_ND_fcns.R') }
-# Dimensions
-if (!(NROW(Z)==NROW(Y))) {
-  stop('The number of rows of X, Y and Z must coincide')
-}
+  stop('Object Y should exist before calling efisegri_ND_fcns.R') }
+
+# Requirements for the IV case
 if (exists('X', mode='numeric')) {
+  # Dimensions
+  if (!(NROW(Z)==NROW(Y))) {
+    stop('The number of rows of X, Y and Z must coincide')
+  }
   if (!(NROW(X) == NROW(Y) & (NROW(Z)==NROW(X)))) {
     stop('Nomber of rows of X must coincide with Z and Y')
   }
-}
-if (!(NROW(Z) == NEQS*T)) {
-  stop('The number of rows of Z does not coincide with the number of moment equations and observations.')
-}
-KZ = NCOL(Z)
-# Order condition
-if(KZ < NPARAMS) {
-  stop('THere are less instruments than parameters. Order condition not satisfied')
-}
+  if (!(NROW(Z) == NEQS*T)) {
+    stop('The number of rows of Z does not coincide with the number of moment equations and observations.')
+  }
+  KZ = NCOL(Z)
+  # Order condition
+  if(KZ < NPARAMS) {
+    stop('THere are less instruments than parameters. Order condition not satisfied')
+  }
+} # IV case
 
 # Initial checks
-if(anyNA(GET_B(PARAMS_CFG$iniVal))) {
+iniV <- PARAMS_CFG$iniVal
+names(iniV) <- PARAMS_CFG$name
+if(anyNA(GET_B(iniV))) {
   stop(paste0("get_b(THETA0) returns NAs in comp_S().  Make sure your GET_B() fcn does what it meant. THETA0 is:", PARAMS_CFG$iniVal))}
 
 ## Verify MODELEQS ----
 walk(MODELEQS, function(thisEq) {
   if (!exists('Y', mode='numeric',  where=thisEq)) {
-    stop('Objects Y and X should exist before calling efisegri_ND_fcns.R') }
-  # Dimensions
-  if (!(NROW(thisEq$Z)==NROW(thisEq$Y))) {
-    stop('The number of rows of X, Y and Z must coincide')
-  }
+    stop('Objects Y should exist before calling efisegri_ND_fcns.R') }
+  # oNLY NECESARY IF iv ESTIMATION
   if (exists('X', mode='numeric', where=thisEq)) {
+    # Dimensions
+    if (!(NROW(thisEq$Z)==NROW(thisEq$Y))) {
+      stop('The number of rows of X, Y and Z must coincide')
+    }
     if (!(NROW(thisEq$X) == NROW(thisEq$Y) & (NROW(thisEq$Z)==NROW(thisEq$X)))) {
       stop('Nomber of rows of X must coincide with Z and Y')
     }
-  }
-  if (!(NROW(thisEq$Z) == T)) {
-    stop('The number of rows of Z does not coincide with the number of moment equations and observations.')
+    if (!(NROW(thisEq$Z) == T)) {
+      stop('The number of rows of Z does not coincide with the number of moment equations and observations.')
+    }
   }
 }) # Verify MODELEQS
 
 
 ## Generate global objects ----
-# Other useful matrices
-I_T = diag(NROW(X))
+
 # X may not exist
 if (exists('X', mode='numeric')) {
+  I_T = diag(NROW(X))
   Xtrans = t(X)
   Mx = I_T - X %*% solve(t(X) %*% X) %*% t(X)
   KX = NCOL(X)
   KZMKX = KZ - KX
   SdF <- (NCOL(Z)-NCOL(X))
 } else {
-  Mx = I_T
+  Mx = diag(NROW(Y))
   KX = 0
   KZMKX = KZ - KX
-  Sdf <- NCOL(Z)
+  Sdf <- ifelse(exists('Z', mode='numeric'), NCOL(Z), 0)
+  Ztrans = NA
 }
-Ztrans = t(Z)
 
 # For Qll computation
 ones_T = rep(1, times=T)
